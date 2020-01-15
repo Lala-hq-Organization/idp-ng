425012cb4b5ea4001e2601cb4d3f3e9bf99a286b
EstherSeyi <sheyiogundijo@gmail.com>
2020-01-15 08:49:26 +0100

    modifies test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+function renderWithRouter(
+  ui,
+  {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] })
+  } = {}
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history
+  };
+}
+
+test("renders learn react link", () => {
+  const { container } = renderWithRouter(<App />);
+  expect(container.innerHTML).toMatch(
+    '<div class="App"><div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"><div class="css-pzdxlx"><div class="css-h7t4y2"></div><div class="css-1hwqqnv"></div></div></div></div>'
+  );
 });
