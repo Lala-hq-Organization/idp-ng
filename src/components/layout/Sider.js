270c0f82a47b7c66a105b99a2dcafe5048d37dc1
EstherSeyi <sheyiogundijo@gmail.com>
2020-01-15 14:14:02 +0100

    modifies layout

--- src/components/layout/Sider.js
+++ src/components/layout/Sider.js
@@ -2,22 +2,34 @@ import React from "react";
 import { Box, Button, Text } from "grommet";
 
 import styles from "./styles";
+import logo from "../../images/logo.png";
 
 const App = () => (
-  <Box
-    gridArea="sidebar"
-    background="#9D52CC"
-    width="small"
-    style={styles.sider}
-  >
-    {["Dashboard", "Candidates", "Camps"].map(name => (
-      <Button key={name} href="#" hoverIndicator>
-        <Box pad={{ horizontal: "medium", vertical: "small" }}>
-          <Text>{name}</Text>
+  <>
+    <Box
+      gridArea="sidebar"
+      background="#9D52CC"
+      width="small"
+      style={styles.sider}
+    >
+      <Box style={styles.logobox}>
+        <Box style={styles.logo}>
+          <img
+            src={logo}
+            alt="logo"
+            style={{ width: "100%", objectFit: "none" }}
+          />
         </Box>
-      </Button>
-    ))}
-  </Box>
+      </Box>
+      {["Dashboard", "Candidates", "Camps"].map(name => (
+        <Button key={name} href="#" hoverIndicator>
+          <Box pad={{ horizontal: "medium", vertical: "small" }}>
+            <Text>{name}</Text>
+          </Box>
+        </Button>
+      ))}
+    </Box>
+  </>
 );
 
 export default App;
--- src/components/layout/styles.js
+++ src/components/layout/styles.js
@@ -8,7 +8,20 @@ const styles = {
   sider: {
     position: "relative",
     zIndex: 0,
-    background: "#9D52CC"
+    background: "#9D52CC",
+    alignItems: "center"
+  },
+  logoBox: {
+    width: "100%",
+    height: "200px"
+  },
+  logo: {
+    width: "91px",
+    height: " 86px",
+    background: "#FFFFFF 0% 0% no-repeat padding-box",
+    border: "5px solid #B065C69F",
+    "border-radius": "28px",
+    opacity: 1
   }
 };
 
