eae1631df4818a862b10abfb578618aa02a3462a
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-01-16 12:56:22 +0100

    Built left details of camps page overview

---
+++ src/pages/Camps/styles.js
@@ -0,0 +1,40 @@
+export const styles = {
+  container: {
+    top: '3.5em',
+    position: 'relative',
+    width: '85%',
+    marginLeft: '3em',
+    borderRadius: '3px',
+    boxShadow: '0px 3px 6px #2C28281C'
+  },
+  firstBox: {
+    width: '35%',
+    background: '#FFFFFF 0% 0% no-repeat padding-box',
+    borderRight: '1px solid #E6E8EA',
+    padding: '1em'
+  },
+  secondBox: {
+    width: '65%',
+    background: '#FFFFFF 0% 0% no-repeat padding-box',
+    padding: '1em'
+  },
+  image: { borderRadius: '50%' },
+  firstBoxTitle: {
+    margin: 0,
+    textTransform: 'uppercase',
+    fontWeight: 400,
+    color: '#000'
+  },
+  firstBoxSubtitle: {
+    margin: 0,
+    fontSize: '1.2rem',
+    fontWeight: 300,
+    color: '#818F90'
+  },
+  dataRow: {
+    fontSize: '1.3rem',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: 0
+  }
+};
