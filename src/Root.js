abe6999d44402777a44a4cfd9cf394c0c733c85a
EstherSeyi <sheyiogundijo@gmail.com>
2020-01-16 09:24:38 +0100

    adds candidate info

--- src/Root.js
+++ src/Root.js
@@ -1,16 +1,20 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Loading from "./components/Loading";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Loading from './components/Loading';
 
 // const Play = lazy(() => import('./components/Play'));
-const Layout = lazy(() => import("./components/layout/Layout"));
+const Layout = lazy(() => import('./components/layout/Layout'));
+const Candidates = lazy(() => import('./components/candidates/CandidateTable'));
 
 const App = () => (
   <BrowserRouter>
     <Suspense fallback={<Loading />}>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Layout />
+        </Route>
+        <Route path="/candidates">
+          <Candidates />
         </Route>
       </Switch>
     </Suspense>
---
+++ src/components/candidates/styles.js
@@ -0,0 +1,33 @@
+export default {
+  mainDiv: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingLeft: '5rem'
+  },
+  table: {
+    backgroundColor: '#ffffff',
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: '0.7rem'
+  },
+  header: {
+    display: 'flex',
+    padding: '1.5rem 3rem'
+  },
+  tableHead: { height: '5rem', backgroundColor: 'red', width: '90%' },
+  tableStyle: {
+    fontFamily: 'Roboto',
+    fontSize: '1rem'
+  },
+  profile: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  profileName: {
+    fontSize: '16px',
+    fontWeight: '800',
+    paddingLeft: '5px'
+  }
+};
