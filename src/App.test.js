ef94f0d8b648f708086261dfd4387a76edacb93f
EstherSeyi <sheyiogundijo@gmail.com>
2020-01-19 16:08:01 +0100

    modifies test

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import store from './store';
+
+import { Provider } from 'react-redux';
+import ErrorBoundary from './ErrorBoundary';
 
 import App from './App';
 
-function renderWithRouter(
-  ui,
-  {
-    route = '/',
-    history = createMemoryHistory({ initialEntries: [route] })
-  } = {}
-) {
-  return {
-    ...render(<Router history={history}>{ui}</Router>),
-    history
-  };
-}
-
-test('renders learn react link', () => {
-  const { container } = renderWithRouter(<App />);
-  expect(container.innerHTML).toMatch(
-    '<div class="App"><div class="StyledGrommet-sc-19lkkz7-0 dwOFwB"><div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"><div class="css-pzdxlx"><div class="css-h7t4y2"></div><div class="css-1hwqqnv"></div></div></div></div></div>'
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>,
+    div
   );
 });
