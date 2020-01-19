dfba6f2051a8af3dc9d0fb0e69eb2a3e755017e9
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-01-19 19:52:05 +0100

    Made homepage responsive; more improvements later

--- src/pages/Homepage/styles.js
+++ src/pages/Homepage/styles.js
@@ -1,6 +1,6 @@
 export const styles = {
   container: {
-    padding: '1em 0em',
+    padding: '1em 0.7em',
     width: '100%',
     maxWidth: '150rem',
     margin: '0 auto'
@@ -8,20 +8,26 @@ export const styles = {
   main: {
     background:
       'transparent radial-gradient(closest-side at 71% 41%, #9D52CC 0%, #4F2966 100%) 0% 0% no-repeat padding-box',
-    height: '70rem'
+    padding: '5em 0em'
   },
-  hero: { color: '#F8FBFD', textShadow: '0px 3px 6px #00000029' },
+  hero: {
+    color: '#F8FBFD',
+    textShadow: '0px 3px 6px #00000029'
+  },
+  heroLeftSection: { flex: '1 1 46%' },
+  heroRightSection: { flex: '0 1 40%' },
   heroTitle: {
     fontSize: '5rem',
     fontFamily: 'Open Sans, sans-serif',
     fontWeight: 400,
-    margin: 0,
     textShadow: '0px 3px 6px #00000029'
   },
   heroText: {
-    width: '43rem',
+    maxWidth: '43rem',
+    width: '100%',
     fontSize: '1.8rem',
     fontWeight: 'bold',
+    marginTop: '0px',
     textShadow: '0px 2px 8px #000000F8'
   },
   loginBtn: {
@@ -29,13 +35,45 @@ export const styles = {
     background: '#FAB315 0% 0% no-repeat padding-box',
     borderColor: '#FAB315',
     borderRadius: '50px',
-    padding: '0.3em 1em',
+    padding: '0.3em 0em',
     fontSize: '2.3rem',
-    fontWeight: 600
+    fontWeight: 600,
+    width: '16rem'
   },
-  mapArea: { padding: '3em 0em' },
+  midSection: {
+    paddingTop: '9em',
+    paddingBottom: '9em',
+    maxWidth: '105rem',
+    fontFamily: 'Open Sans, sans-serif',
+    flexWrap: 'wrap'
+  },
+  ball: {
+    width: '5rem',
+    height: '5rem',
+    borderRadius: '50%',
+    background: '#6EA23C 0% 0% no-repeat padding-box'
+  },
+  purpleBall: {
+    background:
+      'transparent linear-gradient(162deg, #9D52CC 0%, #61337E 100%) 0% 0% no-repeat padding-box',
+    width: '25rem',
+    height: '36rem',
+    zIndex: 2
+  },
+  yellowBall: {
+    background: '#FAB315 0% 0% no-repeat padding-box',
+    width: '15rem',
+    height: '15rem',
+    borderRadius: '50%',
+    zIndex: 1,
+    position: 'absolute',
+    top: '-98px',
+    right: '60px'
+  },
+  mapArea: { paddingTop: '3em', paddingBottom: '3em' },
   footer: { padding: '4em 0em' },
   footerText: {
-    width: '50%'
+    width: '100%',
+    maxWidth: '71rem'
   }
 };
