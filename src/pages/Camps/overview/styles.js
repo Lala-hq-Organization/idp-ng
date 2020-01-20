cfae805c88793c44ef283dad4835acd99ad3de62
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-01-20 16:56:45 +0100

    Fixed the scroll issue

--- src/pages/Camps/overview/styles.js
+++ src/pages/Camps/overview/styles.js
@@ -1,11 +1,14 @@
 export const styles = {
   container: {
-    top: '3.5em',
+    top: '2.5em',
     position: 'relative',
     width: '85%',
     marginLeft: '3em',
-    borderRadius: '3px'
+    borderRadius: '3px',
+    height: '85%',
+    overflowY: 'scroll'
   },
   imageBox: { height: '35em' },
+  image: { width: '100%', objectFit: 'cover' },
   viewBtn: { color: '#7764E4', background: '#fff', padding: '0.2em 2em' }
 };
