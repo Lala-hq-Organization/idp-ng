ccbdafea51ccf7dc62c526fa7308a18e97726cdc
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-01-28 19:26:01 +0100

    shitty changes due to prettier

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,11 @@
-import React from "react";
-import MoonLoader from "react-spinners/MoonLoader";
+import React from 'react';
+import MoonLoader from 'react-spinners/MoonLoader';
 
 const style = {
-  position: "fixed",
-  top: "50%",
-  left: " 50%",
-  transform: "translate(-50%, -50%)"
+  position: 'fixed',
+  top: '50%',
+  left: ' 50%',
+  transform: 'translate(-50%, -50%)'
 };
 
 export default function Loading() {
