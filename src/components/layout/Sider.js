f2af4b00474201f904ac687bb8485965a3e5aedb
EstherSeyi <sheyiogundijo@gmail.com>
2020-02-18 11:48:22 +0100

    adds on mouse over effect to sider

--- src/components/layout/Sider.js
+++ src/components/layout/Sider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, Text } from 'grommet';
 
 import { Link } from 'react-router-dom';
@@ -6,49 +6,73 @@ import list from './list';
 
 import styles from './styles';
 import logo from '../../assets/logo.png';
+import './layout.css';
 
-const Sidebar = () => (
-  <>
-    <Box gridArea="sidebar" width="17em" style={styles.sider}>
-      <Box style={styles.logobox}>
-        <Box style={styles.logo}>
-          <Link to="/">
-            <img
-              src={logo}
-              alt="logo"
-              style={{ width: '100%', objectFit: 'none' }}
-            />
-          </Link>
+const Sidebar = () => {
+  const [isCurrent, setIsCurrent] = useState(null);
+  const [current, setCurrent] = useState(null);
+
+  const handleClick = key => {
+    setCurrent(key);
+    setIsCurrent(true);
+  };
+
+  return (
+    <>
+      <Box gridArea="sidebar" width="17em" style={styles.sider}>
+        <Box style={styles.logobox}>
+          <Box style={styles.logo}>
+            <Link to="/">
+              <img
+                src={logo}
+                alt="logo"
+                style={{ width: '100%', objectFit: 'none' }}
+              />
+            </Link>
+          </Box>
+          <Text color="white" style={styles.adminText}>
+            Admin
+          </Text>
         </Box>
-        <Text color="white" style={styles.adminText}>
-          Admin
-        </Text>
-      </Box>
-      <hr style={{ width: '100%', border: '0.5px solid #fff' }} />
-      <Box style={styles.sideItemBox}>
-        {list.map(item => (
-          <Link to={Object.values(item)[0][0]} key={Object.keys(item)[0]}>
-            <Button key={Object.keys(item)[0]} hoverIndicator>
-              <Box
-                pad={{ horizontal: 'medium', vertical: 'medium' }}
-                direction="row"
+        <hr style={{ width: '100%', border: '0.5px solid #fff' }} />
+        <Box style={styles.sideItemBox}>
+          {list.map(item => (
+            <Link to={Object.values(item)[0][0]} key={Object.keys(item)[0]}>
+              <Button
+                key={Object.keys(item)[0]}
+                // onClic?k
+                onMouseOverCapture={() => handleClick(Object.keys(item)[0])}
               >
-                {Object.values(item)[0][1]}
-                <Text
-                  color="#fff"
-                  alignSelf="start"
-                  margin={{ left: 'small' }}
-                  weight="bold"
+                <Box
+                  pad={{ horizontal: 'medium', vertical: 'medium' }}
+                  direction="row"
                 >
-                  {Object.keys(item)[0]}
-                </Text>
-              </Box>
-            </Button>
-          </Link>
-        ))}
+                  {Object.values(item)[0][1]}
+                  <Text
+                    color={
+                      current === Object.keys(item)[0] && isCurrent
+                        ? '#fff'
+                        : '#CE9FDB'
+                    }
+                    alignSelf="start"
+                    margin={{ left: 'small' }}
+                    style={{
+                      fontWeight:
+                        current === Object.keys(item)[0] && isCurrent
+                          ? 'bold'
+                          : 'light'
+                    }}
+                  >
+                    {Object.keys(item)[0]}
+                  </Text>
+                </Box>
+              </Button>
+            </Link>
+          ))}
+        </Box>
       </Box>
-    </Box>
-  </>
-);
+    </>
+  );
+};
 
 export default Sidebar;
